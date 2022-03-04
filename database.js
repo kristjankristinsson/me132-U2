8ac6fa6e14ccfa78ad2095c56f1e2d15ab019d18
kristjankristinsson <97900321+kristjankristinsson@users.noreply.github.com>
2022-03-04 14:31:43 +0100

    Database
    Created a fruit database with different categories.

---
+++ database.js
@@ -0,0 +1,78 @@
+/*
+
+Fruit DB
+
+*/
+
+let database = [
+    {
+        id: 1,
+        name: "Apple",
+        color: "Red",
+        country: "Sweden",
+        day: 4
+    },
+    {
+        id: 2,
+        name: "Apple",
+        color: "Green",
+        country: "Sweden",
+        day: 8
+    },
+    {
+        id: 3,
+        name: "Grape",
+        color: "Blue",
+        country: "Sweden",
+        day: 3
+    },
+    {
+        id: 4,
+        name: "Kiwi",
+        color: "Green",
+        country: "France",
+        day: 4
+    },
+    {
+        id: 5,
+        name: "Sharon",
+        color: "Orange",
+        country: "Israel",
+        day: 4
+    },
+    {
+        id: 6,
+        name: "Apple",
+        color: "Red",
+        country: "Sweden",
+        day: 4
+    },
+    {
+        id: 7,
+        name: "Grape",
+        color: "Red",
+        country: "Sweden",
+        day: 2
+    },
+    {
+        id: 8,
+        name: "Cherries",
+        color: "Red",
+        country: "Isreal",
+        day: 4
+    },
+    {
+        id: 9,
+        name: "Pear",
+        color: "Green",
+        country: "Sweden",
+        day: 12
+    },
+    {
+        id: 10,
+        name: "Mango",
+        color: "Yellow",
+        country: "Japan",
+        day: 12
+    }
+]
